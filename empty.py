4555ad71ae3473f076f4f6e7df7b4736f7cec6c3
12374040 <semkjaer@hotmail.nl>
2020-07-15 15:34:18 +0200

    .

--- empty.py
+++ empty.py
@@ -1,5 +1,11 @@
 import pyodbc
 
+server = 'ticketscrape.database.windows.net'
+database = 'ts_db'
+username = 'data_admin'
+password = 'Kaasisbaas4'
+driver= '{ODBC Driver 17 for SQL Server}'
+
 conn = pyodbc.connect('DRIVER='+driver+';PORT=1433;SERVER='+server+';PORT=1443;DATABASE='+database+';UID='+username+';PWD='+ password)
 c = conn.cursor()
 
