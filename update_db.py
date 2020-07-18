11f8c036063657b8f9fc01198e82625e01ffb240
robbert <robbert2335@gmail.com>
2020-07-18 20:22:05 +0200

    updated update_db.py with separate links table

--- update_db.py
+++ update_db.py
@@ -13,16 +13,11 @@ def update_database(data):
     # create table if not exists
     c.execute('''IF OBJECT_ID('dbo.ticket_data', 'U') IS NULL
     CREATE TABLE ticket_data (
+    name varchar(255),
     aangeboden int, 
     verkocht int, 
     gezocht int, 
-    name varchar(255), 
     event_date varchar(255), 
-    location varchar(255),
-    city varchar(255),
-    country varchar(255),
-    facebook varchar(255), 
-    link varchar(255),
     timestamp varchar(255)
     );''')
     
@@ -34,15 +29,48 @@ def update_database(data):
                                         verkocht, 
                                         gezocht, 
                                         event_date, 
-                                        location, 
-                                        city,
-                                        country,
-                                        facebook, 
-                                        link, 
                                         timestamp) 
                                     VALUES 
-                                        (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);''', new_values)
+                                        (?, ?, ?, ?, ?, ?);''', new_values)
 
     conn.commit()
     conn.close()
 
+def update_links(links):
+    '''Update db with links'''
+    conn = pyodbc.connect(access)
+    c = conn.cursor()
+
+    c.execute('''IF OBJECT_ID('dbo.link_data', 'U') IS NULL
+    CREATE TABLE link_data (
+    name varchar(255),
+    event_date varchar(255),
+    location varchar(255),
+    city varchar(255),
+    country varchar(255),
+    facebook varchar(255), 
+    link varchar(255),
+    );''')
+
+    # update table
+    new_links = [tuple(row) for row in links.itertuples(index=False)]
+
+    c.execute('''SELECT * FROM link_data''')
+    old_links = [tuple(row) for row in c.itertuples(index=False)]
+
+    links_to_add = [tuple(row) for row in new_links.itertuples(index=False) if row not in old_links]
+
+    # add new links to table
+    c.executemany('''INSERT INTO link_data (
+                                        name,
+                                        event_date,
+                                        location,
+                                        city,
+                                        country,
+                                        facebook,
+                                        link)
+                                    VALUES
+                                        (?, ?, ?, ?, ?, ?, ?);''', links_to_add)
+
+    conn.commit()
+    conn.close()
